cfac5dbbae58e7f22d5e03d6da3304cf1d01a1b8
TamCore <philipp@tamcore.eu>
2012-06-04 01:52:09 +0200

    cpufreq: adaptive: Adding governor adaptive

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -431,6 +431,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
 #endif
 
 
