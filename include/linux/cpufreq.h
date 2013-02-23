07d2ec79ec128d633edb8cd17874ffe2db8b26dd
mohan_kr <mohankr03@gmail.com>
2013-02-23 13:51:25 -0600

    cpufreq: moved samsung ondemand to 'samsung'
    - For enabling std. linux ondemand.
    - Also enabled it in the config file.
    - NOTE: Ramdisk must be changed to enable this governor by default

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -409,6 +409,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAMSUNG)
+extern struct cpufreq_governor cpufreq_gov_samsung;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_samsung)
 #endif
 
 
