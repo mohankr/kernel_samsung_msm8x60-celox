965bc64119b251c54a36937332947449a833fafc
mohan_kr <mohankr03@gmail.com>
2013-04-10 20:18:51 -0600

    cpufreq: lagfree: adding lagfree governor
       TODO:
          enabling MSM_SLEEP_STATS causes a kernel panic, need to investigate.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -442,6 +442,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lagfree)
 #endif
 
 
